1bc8226c4d47abea6a2f9a13ae00dea72f18fe0f
Leo <leon_fritas@hotmail.com>
2024-03-02 00:11:45 -0400

    Imagem de painel adicionada.

--- src/components/painel.jsx
+++ src/components/painel.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ImgPainel from '../assets/images/quebraCabeca.png'
+import ImgPainel from '../assets/images/hortifrutiAlvorada.jpg'
 import styled from 'styled-components'
 import '../App.css'
 import './painel.css'
