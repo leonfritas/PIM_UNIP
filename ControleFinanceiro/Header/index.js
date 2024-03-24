7d11eb17677f8bfdd34ee53f1c20b08b059e1601
Andreo Henrique <129970769+andreowdev@users.noreply.github.com>
2024-03-24 07:31:27 -0400

    Add files via upload

---
+++ ControleFinanceiro/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Container, HeaderStyle, Title} from "./styles";
+const Header = () => {
+  return (
+    <Container>
+      <HeaderStyle>
+        <Title>Controle Financeiro</Title>
+      </HeaderStyle>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ ControleFinanceiro/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: #1d2634;
+`;
+
+export const HeaderStyle = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
